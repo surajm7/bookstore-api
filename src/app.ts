8632ef9730c6528a6490467600b45166177dcf4b
Suraj Gupta <82599987+surajm7@users.noreply.github.com>
2024-09-23 17:17:10 +0530

    add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,19 @@
-import express from "express";
-
+import { config } from "./config/config";
+import express, { NextFunction } from "express";
+import createHttpError, { HttpError } from "http-errors";
+import globalErrorHandler from "./middlewares/globalErrorHandler"
 const app =express()
 
 
 //Routes
 app.get('/',(req,res,next)=>{
-    res.json({message:"welcome book store api"})
-})
+     const error=createHttpError(400,"something went wrong");
+    throw error;
 
+    res.json({message:"welcome book store api"})
+});
+
+//global error handler
+app.use(globalErrorHandler);
 export default app;
 

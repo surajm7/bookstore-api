b42259cd53499ca271a744034c35e6068bd526b3
Suraj Gupta <82599987+surajm7@users.noreply.github.com>
2024-09-24 00:01:28 +0530

    add login user handler

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -57,5 +57,44 @@ const createUser = async(req:Request,res:Response,next:NextFunction)=>{
 
 };
 
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+    const { email, password } = req.body;
+  
+    if (!email || !password) {
+      return next(createHttpError(400, "All fields are required"));
+    }
+  
 
+    //Database call
+    let user: User | null;
+    try {
+        
+        user = await userModel.findOne({ email });
+        if (!user) {
+          return next(createHttpError(404, "User not found."));
+        }
+      
+        const isMatch = await bcrypt.compare(password, user.password);
+      
+        if (!isMatch) {
+          return next(createHttpError(400, "Username or password incorrect!"));
+        }
+      
+    } catch (error) {
+        return next(createHttpError(500,"Error Username or password incorrect!"))
+    }
+
+    try {
+        
+        const token = sign({ sub: user._id }, config.jwtSecret as string, {
+            expiresIn: "7d",
+            algorithm: "HS256",
+          });
+        
+          res.json({ accessToken: token });
+    } catch (error) {
+        return next(createHttpError(500,"Error while signing the jwt token"))
+    }
+  
+  };
 export {createUser,loginUser};

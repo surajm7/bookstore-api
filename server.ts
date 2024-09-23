65e7a9345ecde1239a8db86b4240e000bb1ffce0
Suraj Gupta <82599987+surajm7@users.noreply.github.com>
2024-09-23 16:43:28 +0530

    setup db

--- server.ts
+++ server.ts
@@ -1,7 +1,11 @@
+import { config } from "./src/config/config";
 import app from "./src/app";
+import connectDB from "./src/config/db";
 
-const startServer =()=>{
-    const port=process.env.PORT || 3000;
+const startServer =async()=>{
+     // Connect database
+  await connectDB();
+    const port= config.port || 3000;
 
     app.listen(port,()=>{
         console.log(`Listening on port: ${port}`);

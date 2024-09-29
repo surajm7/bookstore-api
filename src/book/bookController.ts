845a41e42d84b643e253314f1cbdac01bf397169
Suraj Gupta <82599987+surajm7@users.noreply.github.com>
2024-09-29 21:18:17 +0530

    add book model

---
+++ src/book/bookController.ts
@@ -0,0 +1,12 @@
+
+import { Request, Response, NextFunction } from "express";
+
+
+
+const createBook = async (req: Request, res: Response, next: NextFunction) => {
+   res.json({});
+};
+
+
+
+export { createBook };
